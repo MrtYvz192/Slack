d1a162c35db0bbb9902ca3fa5e914b833432a236
MrtYvz192 <wingsoverthesky@gmail.com>
2020-05-13 17:59:43 +0300

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,6 +3,10 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able create channels");
+        
+        if(5>3){ // created in gitHub
+            System.out.println("5 is greater than 3")
+        }
     }
 
 }
